03da09ca451d4d491d0beaba9653d945f0749141
Cretu Mircea <mcs@mcs1.(none)>
2010-07-13 14:32:06 +0200

    add types.h

---
+++ src/types.h
@@ -0,0 +1,21 @@
+#ifndef _RIMRES_FIPAACL_MESSAGEGENERATOR_TYPES_
+#define _RIMRES_FIPAACL_MESSAGEGENERATOR_TYPES_
+
+namespace fipa {
+namespace acl {
+    
+    typedef std::string Ontology;
+    typedef std::string Language;
+    typedef std::string Content;
+    typedef std::string Protocol;
+    typedef std::string Performative;
+    typedef std::string ConversationID;
+    typedef std::string Encoding;
+    typedef std::string ReplyBy;
+    typedef std::string InReplyTo;
+    typedef std::string ReplyWith;
+    
+} // end of acl
+} // end of fipa
+
+#endif
