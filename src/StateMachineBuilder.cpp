b8dbd4bdd0a87e4563add0ad692e8c4aa169c2db
Cretu Mircea <mcs@mcs1.(none)>
2011-01-18 18:07:51 +0100

    added dependency to tinyxml in manifest.xml corrected compile errors in StateMachineBuilder

--- src/StateMachineBuilder.cpp
+++ src/StateMachineBuilder.cpp
@@ -1,16 +1,19 @@
 #include "StateMachineBuilder.h"
-#include <tinyxml.h>
-#include <tinystr.h>
 
+#include <iostream>
 #include <algorithm>
 #include <cstring>
 
-const std::string StateMachineBuilder::from = std::string("from");
-const std::string StateMachineBuilder::to = std::string("to");
-const std::string StateMachineBuilder::target = std::string("target");
-const std::string StateMachineBuilder::id = std::string("id");
-const std::string StateMachineBuilder::final = std::string("final");
-const std::string StateMachineBuilder::performative = std::string("performative");
+namespace fipa {
+namespace acl {
+
+const std::string StateMachineBuilder::from = 		std::string("from");
+const std::string StateMachineBuilder::to = 		std::string("to");
+const std::string StateMachineBuilder::target = 		std::string("target");
+const std::string StateMachineBuilder::id = 		std::string("id");
+const std::string StateMachineBuilder::final = 		std::string("final");
+const std::string StateMachineBuilder::performative = 	std::string("performative");
+const std::string StateMachineBuilder::initial = 		std::string("initial");
 
 StateMachineBuilder::StateMachineBuilder()
 {
@@ -20,7 +23,7 @@ StateMachineBuilder::StateMachineBuilder()
 
 StateMachine StateMachineBuilder::loadSpecification(std::string infile)
 {
-    TiXmlDocument file = TiXmlDocument(infile);
+    TiXmlDocument file = TiXmlDocument(infile.c_str());
     
     if (!file.LoadFile())
     {
@@ -38,8 +41,9 @@ StateMachine StateMachineBuilder::parseStateMachineNode(TiXmlElement *sm)
 {
     StateMachine retSM = StateMachine();
     
-    char *value = NULL;
-    value = sm->Attribute(StateMachineBuilder::initial.c_str())
+    const char *value;
+    char *aux = strcpy(aux,StateMachineBuilder::initial.c_str());
+    value = sm->Attribute(StateMachineBuilder::initial.c_str());
     if (value)
         retSM.setInitialState(std::string(value));
     else;
@@ -47,7 +51,7 @@ StateMachine StateMachineBuilder::parseStateMachineNode(TiXmlElement *sm)
         
     TiXmlHandle handleSm = TiXmlHandle(sm);
     TiXmlElement* child = handleSm.FirstChild( "state" ).ToElement();
-    parseStates(child,retSM);
+    addStates(child,retSM);
     
     addInvolvedAgentsMap(retSM);
     
@@ -58,15 +62,15 @@ State StateMachineBuilder::parseStateNode(TiXmlElement *st)
 {
     State ret;
     
-    char *value = NULL;
-    value = st->Attribute(StateMachineBuilder::id.c_str())
+    const char *value;
+    value = st->Attribute(StateMachineBuilder::id.c_str());
     //if (value) ---- should not be necessary to check because it is already checked in the caller function(parseStates())
         ret.setUID(std::string(value));
     //else;
         //TODO: throw some exception here
     
     value = NULL;
-    value = st->Attribute(StateMachineBuilder::final.c_str())
+    value = st->Attribute(StateMachineBuilder::final.c_str());
     if (value)
     {
         if (!strcmp(value,"yes"))
@@ -80,7 +84,7 @@ State StateMachineBuilder::parseStateNode(TiXmlElement *st)
         
     TiXmlHandle handleState = TiXmlHandle(st);
     TiXmlElement *trans = handleState.FirstChild("transition").ToElement();
-    for (trans; trans; trans = transition->NextSiblingElement("transition") )
+    for (trans; trans; trans = trans->NextSiblingElement("transition") )
     {
         Transition t = parseTransitionNode(trans);
         ret.addTransition(t);
@@ -92,11 +96,12 @@ State StateMachineBuilder::parseStateNode(TiXmlElement *st)
 void StateMachineBuilder::addStates(TiXmlElement *st,StateMachine &sm)
 {
     TiXmlElement *next;
-    char *value = NULL;
-    value = st->Attribute(StateMachineBuilder::id.c_str())
+    const char *value = NULL;
+    value = st->Attribute(StateMachineBuilder::id.c_str());
     if (value)
     {
-        if ( (std::vector<std::string>::iterator it = find(states.begin(), states.end(), std::string(value))) == states.end() )
+        std::vector<std::string>::iterator it = find(states.begin(), states.end(), std::string(value));
+        if ( it == states.end() )
 	  states.push_back(std::string(value));
         else;
 	  //TODO: throw some exception here(duplicate state)
@@ -104,9 +109,9 @@ void StateMachineBuilder::addStates(TiXmlElement *st,StateMachine &sm)
     else;
         //TODO: throw some exception here
     
-    if ( (next = st->nextSilingElement("state")) != NULL )
+    if ( (next = st->NextSiblingElement("state")) != NULL )
     {
-        parseStates(next);
+        addStates(next,sm);
         sm.addState(parseStateNode(st));        
     }
     else
@@ -116,12 +121,13 @@ void StateMachineBuilder::addStates(TiXmlElement *st,StateMachine &sm)
 Transition StateMachineBuilder::parseTransitionNode(TiXmlElement *trans)
 {
     Transition ret;
-    char *value = NULL;
+    const char *value = NULL;
     value = trans->Attribute(StateMachineBuilder::from.c_str());
     if ( value )
     {
+        std::vector<std::string>::iterator it = find (roles.begin(), roles.end(), std::string(value));
         ret.setFrom(std::string(value));
-        if ( (std::vector<std::string>::iterator it = find (roles.begin(), roles.end(), std::string(value))) == roles.end())
+        if ( it == roles.end())
 	  roles.push_back(std::string(value));
     }
     else;
@@ -132,17 +138,19 @@ Transition StateMachineBuilder::parseTransitionNode(TiXmlElement *trans)
     if (value)
     {
         ret.setTo(std::string(value));
-        if ( (std::vector<std::string>::iterator it = find (roles.begin(), roles.end(), std::string(value))) == roles.end())
+        std::vector<std::string>::iterator it = find (roles.begin(), roles.end(), std::string(value));
+        if ( it == roles.end())
 	  roles.push_back(std::string(value));
     }
     else;
         //TODO: throw some exception here
     
     value = NULL;
-    value = trans->Attribute(StateMachineBuilder::target.c_str())
+    value = trans->Attribute(StateMachineBuilder::target.c_str());
     if (value)
     {
-        if ((std::vector<std::string>::iterator it = find(states.begin(), states.end(), std::string(value))) != states.end() )
+        std::vector<std::string>::iterator it = find(states.begin(), states.end(), std::string(value));
+        if ( it != states.end() )
 	  ret.setNextStateName(std::string(value));
         else;
 	  //TODO: throw some exception here(no such state in the spec file)
@@ -151,7 +159,7 @@ Transition StateMachineBuilder::parseTransitionNode(TiXmlElement *trans)
         //TODO: throw some exception here
 
     value = NULL;
-    value = trans->Attribute(StateMachineBuilder::performative.c_str())
+    value = trans->Attribute(StateMachineBuilder::performative.c_str());
     if (value)
         ret.setExpectedPerformative(std::string(value));
     else;
@@ -164,7 +172,10 @@ void StateMachineBuilder::addInvolvedAgentsMap(StateMachine &sm)
     std::vector<std::string>::iterator it;
     for (it = roles.begin(); it != roles.end(); it++)
     {
-        newrole = Role(*it)
+        std::string newrole = Role(*it);
         sm.addRole(newrole);
     }
 }
+
+} //end of namespace acl
+} //end of namespace fipa
