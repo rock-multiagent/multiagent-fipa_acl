208085a01954d3391d4ad10a85d12e885b4d8274
Thomas Roehr <thomas.roehr@dfki.de>
2011-04-19 12:39:52 +0200

    Remove generator from manifest

---
+++ src/bitefficient_message.h
@@ -0,0 +1,7 @@
+#ifndef _FIPA_ACL_BITEFFICIENT_MESSAGE_H_
+#define _FIPA_ACL_BITEFFICIENT_MESSAGE_H_
+
+#include <fipa_acl/message_generator/message_generator.h>
+#include <fipa_acl/message_parser/message_parser.h>
+
+#endif // _FIPA_ACL_BITEFFICIENT_MESSAGE_H_ 
