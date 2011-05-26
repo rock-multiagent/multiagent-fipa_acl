e914ee94e40ef175b284e02d5c39fed00a6d0c31
Thomas Roehr <thomas.roehr@dfki.de>
2011-05-26 17:16:15 +0200

    Add comments to types

--- src/types.h
+++ src/types.h
@@ -7,16 +7,30 @@ namespace fipa {
 namespace acl {
     
     typedef std::string Role;
-    
+   
+    // Role is the generic name of the actor
+    // such as 'initiator' (whereas 'initiator')
+    // is a keyword 
+    // AgentMapping is assigned once the conversation
+    // has started an the agents role has been identified
+    // 
+    // check allows to detect whether a role has already
+    // been assigned (defaults to false)
     typedef struct {
         Role role;
         AgentID agent;
         bool check;
     } AgentMapping;
     
+    // Specific for subprotocol in order to match
+    // roles across different statemachines, i.e.
+    // from parent statemachine (master) to the 
+    // enclosed one (resident) 
     typedef struct {
         Role master;
         Role resident;
+        // whether or not it has already been assigned
+        // in the current statemachine (resident)
         bool check;
     } RoleCorrelation;
     
