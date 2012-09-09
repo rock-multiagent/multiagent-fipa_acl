7a294db7c6526801b2f2e608d025c817e16e372a
Thomas Roehr <thomas.roehr@dfki.de>
2012-09-09 15:00:52 +0200

    Update conversation monitor header

--- src/conversation_monitor.h
+++ src/conversation_monitor.h
@@ -5,5 +5,7 @@
 #include <fipa_acl/conversation_monitor/transition.h>
 #include <fipa_acl/conversation_monitor/state.h>
 #include <fipa_acl/conversation_monitor/statemachine.h>
+#include <fipa_acl/conversation_monitor/statemachine_reader.h>
+#include <fipa_acl/conversation_monitor/statemachine_factory.h>
 
 #endif // _FIPA_ACL_CONVERSATION_MONITOR_H_
