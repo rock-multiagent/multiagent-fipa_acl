be4dadf6869e3fa19b9ca662ff3e09b777a605e6
Thomas Roehr <thomas.roehr@dfki.de>
2012-09-09 15:01:41 +0200

    Update statemachine tests

--- test/unit-testing/conversation_monitor/statemachine_test.cpp
+++ test/unit-testing/conversation_monitor/statemachine_test.cpp
@@ -28,14 +28,20 @@ BOOST_AUTO_TEST_CASE(transition_test)
     BOOST_REQUIRE(t.getSenderRole() == senderRole);
     BOOST_REQUIRE(t.getReceiverRole() == receiverRole);
     BOOST_REQUIRE(t.getPerformative() == performative);
-    BOOST_REQUIRE(t.getSourceState() == sourceStateId);
-    BOOST_REQUIRE(t.getTargetState() == targetStateId);
+    BOOST_REQUIRE(t.getSourceStateId() == sourceStateId);
+    BOOST_REQUIRE(t.getTargetStateId() == targetStateId);
+
+    Transition t0(senderRole, receiverRole, performative, sourceStateId, targetStateId);
+
+    BOOST_REQUIRE(t0.getSenderRole() == senderRole);
+    BOOST_REQUIRE(t0.getReceiverRole() == receiverRole);
+    BOOST_REQUIRE(t0.getPerformative() == performative);
+    BOOST_REQUIRE(t0.getSourceStateId() == sourceStateId);
+    BOOST_REQUIRE(t0.getTargetStateId() == targetStateId);
 
     ACLMessage inputMsg(performative);
     ACLMessage validatorMsg(performative);
     RoleMapping roleMapping;
-    // Should fail, since the roles sender and receiver are not set
-    BOOST_REQUIRE_THROW( t.validateMessage(inputMsg, validatorMsg, roleMapping), std::runtime_error );
 
     AgentID self(".*");
     AgentID other(".*");
@@ -44,11 +50,12 @@ BOOST_AUTO_TEST_CASE(transition_test)
     roleMapping.addRole(receiverRole);
     roleMapping.addExpectedAgent(senderRole, self);
     roleMapping.addExpectedAgent(receiverRole, other);
+    // Failing since list of receivers is empty
+    BOOST_REQUIRE_THROW( t.validateMessage(inputMsg, validatorMsg, roleMapping), std::runtime_error );
+    inputMsg.addReceiver(other);
     // Check whether current message conforms to roleMapping -- should not matter since all fields 
     // are empty
-    BOOST_REQUIRE( t.validateMessage(inputMsg, validatorMsg, roleMapping) );
-
-
+    BOOST_REQUIRE( t.validateMessage(inputMsg, validatorMsg, roleMapping));
 
     // Prepare the current role mapping -- as it should be, i.e. expected
     roleMapping.setSelf(self);
@@ -64,6 +71,114 @@ BOOST_AUTO_TEST_CASE(transition_test)
     // sender and receiver are taken from the roleMapping, i.e. sender receiver field is ignored from the 
     // validator message
     BOOST_REQUIRE( t.validateMessage(inputMsg, validatorMsg, roleMapping) );
+
+    //--------------- Testing flags
+
+    roleMapping.clearExpectedAgents(senderRole);
+    roleMapping.clearExpectedAgents(receiverRole);
+    //
+    // Should fail, since the roles sender and receiver are not set
+    ACLMessage input;
+    ACLMessage validator;
+    BOOST_REQUIRE_THROW( t.validateMessage(input, validator, roleMapping, validation::RECEIVERS), std::runtime_error );
+
+    BOOST_REQUIRE( t.validateMessage(inputMsg, validatorMsg, roleMapping, validation::NONE) );
+
+    // Test performative
+    inputMsg.setPerformative(ACLMessage::INFORM);
+    validatorMsg.setPerformative(ACLMessage::REQUEST);
+    BOOST_REQUIRE(! t.validateMessage(inputMsg, validatorMsg, roleMapping, validation::PERFORMATIVE) );
+
+    validatorMsg.setPerformative(ACLMessage::INFORM);
+    BOOST_REQUIRE(t.validateMessage(inputMsg, validatorMsg, roleMapping, validation::PERFORMATIVE) );
+    validatorMsg.setPerformative(ACLMessage::REQUEST);
+
+    std::string testString = "test";
+    std::string testOtherString = "test-other";
+
+    AgentID senderAgentID("senderAgent");
+    AgentID receiverAgentID("receiverAgent");
+    // Test sender
+
+    inputMsg.setSender(senderAgentID);
+    roleMapping.addExpectedAgent(t.getSenderRole(), receiverAgentID);
+    BOOST_REQUIRE(! t.validateMessage(inputMsg, validatorMsg, roleMapping, validation::SENDER));
+
+    roleMapping.clearExpectedAgents(t.getSenderRole());
+    roleMapping.addExpectedAgent(t.getSenderRole(),senderAgentID);
+    BOOST_REQUIRE(t.validateMessage(inputMsg, validatorMsg, roleMapping, validation::SENDER));
+    roleMapping.clearExpectedAgents(t.getSenderRole());
+
+    // Test receiver
+    roleMapping.addExpectedAgent(t.getReceiverRole(), receiverAgentID);
+    roleMapping.addExpectedAgent(t.getReceiverRole(), senderAgentID);
+    // Event though the list of receiver agent might be a subset of the expected ones
+    // the statemachine has to make sure that before a state transition takes place 
+    // all potential receivers have been dealt with
+    inputMsg.clearReceivers();
+    inputMsg.addReceiver(senderAgentID);
+    BOOST_REQUIRE(t.validateMessage(inputMsg, validatorMsg, roleMapping, validation::RECEIVERS));
+
+    inputMsg.addReceiver(senderAgentID);
+    roleMapping.addExpectedAgent(t.getReceiverRole(), senderAgentID);
+
+    BOOST_REQUIRE(t.validateMessage(inputMsg, validatorMsg, roleMapping, validation::RECEIVERS));
+    roleMapping.clearExpectedAgents(t.getReceiverRole());
+
+    // Test ConversationID flag
+    inputMsg.setConversationID(testString);
+    validatorMsg.setConversationID(testOtherString);
+    BOOST_REQUIRE(! t.validateMessage(inputMsg, validatorMsg, roleMapping, validation::CONVERSATION_ID) );
+
+    validatorMsg.setConversationID(testString);
+    BOOST_REQUIRE(t.validateMessage(inputMsg, validatorMsg, roleMapping, validation::CONVERSATION_ID) );
+    validatorMsg.setConversationID(testOtherString);
+
+    // Test protocol flag
+    inputMsg.setProtocol(testString);
+    validatorMsg.setProtocol(testOtherString);
+    BOOST_REQUIRE(! t.validateMessage(inputMsg, validatorMsg, roleMapping, validation::PROTOCOL) );
+
+    validatorMsg.setProtocol(testString);
+    BOOST_REQUIRE(t.validateMessage(inputMsg, validatorMsg, roleMapping, validation::PROTOCOL) );
+    validatorMsg.setProtocol(testOtherString);
+
+    // Test encoding flag
+    inputMsg.setEncoding(testString);
+    validatorMsg.setEncoding(testOtherString);
+    BOOST_REQUIRE(! t.validateMessage(inputMsg, validatorMsg, roleMapping, validation::ENCODING) );
+
+    validatorMsg.setEncoding(testString);
+    BOOST_REQUIRE(t.validateMessage(inputMsg, validatorMsg, roleMapping, validation::ENCODING) );
+    validatorMsg.setEncoding(testOtherString);
+
+    // Test Language flag
+    inputMsg.setLanguage(testString);
+    validatorMsg.setLanguage(testOtherString);
+    BOOST_REQUIRE(! t.validateMessage(inputMsg, validatorMsg, roleMapping, validation::LANGUAGE) );
+
+    validatorMsg.setLanguage(testString);
+    BOOST_REQUIRE(t.validateMessage(inputMsg, validatorMsg, roleMapping, validation::LANGUAGE) );
+    validatorMsg.setLanguage(testOtherString);
+
+    // Test Ontology flag
+    inputMsg.setOntology(testString);
+    validatorMsg.setOntology(testOtherString);
+    BOOST_REQUIRE(! t.validateMessage(inputMsg, validatorMsg, roleMapping, validation::ONTOLOGY) );
+
+    validatorMsg.setOntology(testString);
+    BOOST_REQUIRE(t.validateMessage(inputMsg, validatorMsg, roleMapping, validation::ONTOLOGY) );
+    validatorMsg.setOntology(testOtherString);
+
+    // Test InReplyTo flag
+    inputMsg.setInReplyTo(testString);
+    validatorMsg.setInReplyTo(testOtherString);
+    BOOST_REQUIRE(! t.validateMessage(inputMsg, validatorMsg, roleMapping, validation::IN_REPLY_TO) );
+
+    validatorMsg.setInReplyTo(testString);
+    BOOST_REQUIRE(t.validateMessage(inputMsg, validatorMsg, roleMapping, validation::IN_REPLY_TO) );
+    validatorMsg.setInReplyTo(testOtherString);
+
 }
 
 BOOST_AUTO_TEST_CASE(state_test)
@@ -80,10 +195,20 @@ BOOST_AUTO_TEST_CASE(state_test)
     BOOST_REQUIRE(state.getId() == otherStateId);
 
     BOOST_REQUIRE(!state.isFinal());
+    // non-final and no transitions defined - invalid state
+    BOOST_REQUIRE_THROW(state.generateDefaultTransitions(), std::runtime_error);
 
-    // Test for default transitions
+    RoleId senderRoleId = "sender";
+    RoleId receiverRoleId = "receiver";
+    Role senderRole(senderRoleId);
+    Role receiverRole(receiverRoleId);
+    Transition t(senderRole, receiverRole, ACLMessage::INFORM, stateId, otherStateId);
+    state.addTransition(t);
+    state.generateDefaultTransitions();
+
+    // Test for default transitions being added
     std::vector<Transition> transitions = state.getTransitions();
-    BOOST_REQUIRE(!transitions.empty());
+    BOOST_REQUIRE(transitions.size() > 1);
 
     // Verify final state
     FinalState finalState(stateId);
@@ -91,6 +216,27 @@ BOOST_AUTO_TEST_CASE(state_test)
 
     transitions = finalState.getTransitions();
     BOOST_REQUIRE(transitions.empty());
+}
+
+
+BOOST_AUTO_TEST_CASE(statemachine_reader_test)
+{
+    using namespace fipa::acl;
+    char buffer[1024];
+    BOOST_REQUIRE_MESSAGE( readlink("/proc/self/exe", buffer, 1024) != -1, "Retrieving current execution path");
+    std::string str(buffer);
+    std::string executionDir = str.substr(0, str.rfind('/'));
+    // Assuming we have do a build into build/ parallel to src/ 
+    std::string configurationPath = executionDir + "/../../../../configuration/protocols";
+
+    StateMachineReader reader;
+    BOOST_CHECK_MESSAGE(1, "NOTE: Assuming a this test resides in a build directory parallel to the folder configuration/");
+    BOOST_REQUIRE_NO_THROW(reader.loadSpecification(configurationPath + "/inform"));
+    BOOST_REQUIRE_NO_THROW(reader.loadSpecification(configurationPath + "/dutchAuction"));
+    BOOST_REQUIRE_NO_THROW(reader.loadSpecification(configurationPath + "/subscribe"));
+
+    StateMachine statemachine = reader.loadSpecification(configurationPath + "/inform");
+
 }
 BOOST_AUTO_TEST_SUITE_END()
 
