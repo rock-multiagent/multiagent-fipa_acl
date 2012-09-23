d3b8079d7f7c1f3e073aedd8322751d14ba3eca1
Thomas Roehr <thomas.roehr@dfki.de>
2012-09-23 22:54:07 +0200

    Adding explicit template instanciation

--- src/message_parser/grammar_bitefficient.cpp
+++ src/message_parser/grammar_bitefficient.cpp
@@ -17,4 +17,21 @@ phoenix::function<convertToCharVectorImpl> convertToCharVector;
 phoenix::function<convertToNativeShortImpl> lazy_ntohs;
 phoenix::function<convertToNativeLongImpl> lazy_ntohl;
 
-}
+typedef std::string::const_iterator iterator_type;
+
+namespace acl {
+namespace bitefficient {
+
+template struct CodedNumber<iterator_type>;
+template struct DateTime<iterator_type>;
+template struct BinDateTime<iterator_type>;
+template struct Index<iterator_type>;
+template struct Word<iterator_type>;
+template struct BinWord<iterator_type>;
+template struct BinExpression<iterator_type>;
+template struct AgentIdentifier<iterator_type>;
+template struct message_grammar<iterator_type>;
+
+} // end namespace bitefficient
+} // end namespace acl
+} // end namespace fipa
