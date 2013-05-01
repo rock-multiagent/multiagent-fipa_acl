9ae83d9965e5b0dc73a704c9e772c0eef911bdca
Thomas Roehr <thomas.roehr@dfki.de>
2013-05-01 13:43:08 +0200

    Add SerializedLetter to facilitate transport

---
+++ src/message_generator/serialized_letter.h
@@ -0,0 +1,61 @@
+#ifndef FIPA_ACL_SERIALIZED_LETTER_H
+#define FIPA_ACL_SERIALIZED_LETTER_H
+
+#include <stdint.h>
+
+#include <string>
+#include <vector>
+#include <base/time.h>
+#include <fipa_acl/fipa_acl.h>
+
+namespace fipa {
+
+/**
+ * Facilitate transport
+ */
+struct SerializedLetter 
+{
+    SerializedLetter();
+
+    /**
+     */
+    SerializedLetter(const fipa::acl::Letter& letter, fipa::acl::representation::Type representation);
+
+    /**
+     * Clear the serialization data
+     */
+    inline void clear() { representation = fipa::acl::representation::UNKNOWN; data.clear(); }
+
+    /**
+     * Retrieve the underlying vector
+     */
+    inline std::vector<uint8_t> getVector() const { return this->data; }
+
+    /**
+     * Set the serialized content
+     */
+    void setData(const std::string& msg);
+
+    /**
+     * Retrieve the size of the serialized content
+     */
+    inline int size() const { return this->data.size(); }
+
+    /**
+     * Get data in string container
+     */
+    std::string getDataAsString() const;
+
+    /**
+     * Retrieve decoded letter
+     */
+    fipa::acl::Letter getLetter() const;
+
+    fipa::acl::representation::Type representation;
+    std::vector<uint8_t> data;
+    base::Time timestamp;
+};
+
+} // namespace fipa
+#endif
+
