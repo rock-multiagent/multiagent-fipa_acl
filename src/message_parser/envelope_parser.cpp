6014c22c9a946c861389df2513568bbc10bfdaa8
Thomas Roehr <thomas.roehr@dfki.de>
2013-06-11 21:16:09 +0200

    Parse envelope directly into given object

--- src/message_parser/envelope_parser.cpp
+++ src/message_parser/envelope_parser.cpp
@@ -9,10 +9,9 @@ bool EnvelopeParser::parseData(const std::string& storage, ACLEnvelope& envelope
     typedef fipa::acl::bitefficient::Envelope<std::string::const_iterator> bitefficient_envelope_grammar;
     bitefficient_envelope_grammar grammar;
 
-    fipa::acl::ACLEnvelope parseTree;
     std::string::const_iterator iter = storage.begin(); 
     std::string::const_iterator end = storage.end(); 
-    bool r = parse(iter, end, grammar, parseTree);
+    bool r = parse(iter, end, grammar, envelope);
 
     if(r && iter == end)
     {
