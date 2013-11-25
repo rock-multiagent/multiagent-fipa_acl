e8b57f996dfd1cc143c6499c88b876b371940159
Thomas Roehr <thomas.roehr@dfki.de>
2013-11-25 12:16:57 +0100

    test: allow testing for grammars with skipper

--- test/unit-testing/messages/test_utils.h
+++ test/unit-testing/messages/test_utils.h
@@ -3,6 +3,9 @@
 
 #include <boost/test/auto_unit_test.hpp>
 #include <string>
+#include <boost/spirit/include/qi.hpp>
+
+namespace qi = boost::spirit::qi;
 
 template< template<class> class TIn, typename TOut>
 TOut testGrammar(const std::string& storage, bool expectedSuccess = true)
@@ -16,9 +19,9 @@ TOut testGrammar(const std::string& storage, bool expectedSuccess = true)
     bool r = parse(iter, end, grammar, parseTree);
     if(expectedSuccess)
     {
-    	BOOST_REQUIRE_MESSAGE(r, "Test Grammar with string of size: " << storage.size());
+        BOOST_REQUIRE_MESSAGE(r, "Test Grammar with string of size: " << storage.size());
     } else {
-    	BOOST_REQUIRE_MESSAGE(!r, "Test Grammar with string of size: " << storage.size());
+        BOOST_REQUIRE_MESSAGE(!r, "Test Grammar with string of size: " << storage.size());
     }
     return parseTree;
 }
@@ -27,6 +30,39 @@ template< template<class> class TIn, typename TOut>
 void testFailGrammar(const std::string& storage)
 {
     typedef TIn<std::string::const_iterator> grammar_type;
+    grammar_type grammar;
+    TOut parseTree;
+
+    std::string::const_iterator iter = storage.begin();
+    std::string::const_iterator end = storage.end();
+    bool r = parse(iter, end, grammar, parseTree);
+
+    BOOST_REQUIRE(!r);
+}
+
+template< template<class Iterator, class Skipper> class TIn, typename TOut>
+TOut testGrammarWithSkipper(const std::string& storage, bool expectedSuccess = true)
+{
+    typedef TIn<std::string::const_iterator, qi::unused_type> grammar_type;
+    grammar_type grammar;
+    TOut parseTree;
+
+    std::string::const_iterator iter = storage.begin();
+    std::string::const_iterator end = storage.end();
+    bool r = parse(iter, end, grammar, parseTree);
+    if(expectedSuccess)
+    {
+        BOOST_REQUIRE_MESSAGE(r, "Test Grammar with string of size: " << storage.size());
+    } else {
+        BOOST_REQUIRE_MESSAGE(!r, "Test Grammar with string of size: " << storage.size());
+    }
+    return parseTree;
+}
+
+template< template<class Iterator, class Skipper> class TIn, typename TOut>
+void testFailGrammarWithSkipper(const std::string& storage)
+{
+    typedef TIn<std::string::const_iterator, qi::unused_type> grammar_type;
     grammar_type grammar;
     TOut parseTree;
 
