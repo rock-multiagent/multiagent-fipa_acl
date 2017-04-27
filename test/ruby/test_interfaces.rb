a73360c54f549dfefdc0c260f23864f1248aa340
Thomas Roehr <thomas.roehr@dfki.de>
2017-04-27 11:26:41 +0200

    ruby: reformat test_interfaces.rb

--- test/ruby/test_interfaces.rb
+++ test/ruby/test_interfaces.rb
@@ -20,80 +20,80 @@ class FipaMessageTest < Minitest::Test
         msg.setConversationID("test-conversation-id")
     end
 
-	def test_UserdefinedParameter
-		name = "user-def-param"
-		value = "user-def-value"
-		param = UserDefinedParameter.new 
+    def test_UserdefinedParameter
+        name = "user-def-param"
+        value = "user-def-value"
+        param = UserDefinedParameter.new
                 param.setName name
-		param.setValue value
+        param.setValue value
 
-		assert_equal(name, param.getName)	
-		assert_equal(value, param.getValue)
-	end
+        assert_equal(name, param.getName)
+        assert_equal(value, param.getValue)
+    end
 
-	def test_AgentId
-		name = "agent-id"
-		addresses = []
-		10.times do |count|
-			addresses[count] = "address-#{count}"
-		end
+    def test_AgentId
+        name = "agent-id"
+        addresses = []
+        10.times do |count|
+            addresses[count] = "address-#{count}"
+        end
 
-		resolvers = []
-		10.times do |count|
-			resolver = AgentId.new("resolver-#{count}")
-			# add an inner resolver
-			iResolver = AgentId.new("inner-resolver-#{count}")
-			resolver.addResolver(iResolver)
+        resolvers = []
+        10.times do |count|
+            resolver = AgentId.new("resolver-#{count}")
+            # add an inner resolver
+            iResolver = AgentId.new("inner-resolver-#{count}")
+            resolver.addResolver(iResolver)
 
-			resolvers[count] = resolver
-		end
+            resolvers[count] = resolver
+        end
 
-		# This is the single object to start with
-		aid = AgentId.new(name)
-		addresses.each do |address|
-			aid.addAddress(address)
-		end
+        # This is the single object to start with
+        aid = AgentId.new(name)
+        addresses.each do |address|
+            aid.addAddress(address)
+        end
 
-		resolvers.each do |resolver|
-			aid.addResolver(resolver)
-		end
+        resolvers.each do |resolver|
+            aid.addResolver(resolver)
+        end
 
-		# now check the object
-		aidAddresses = aid.getAddresses
-		aidAddresses = aidAddresses.reverse
-		count = 0
-		addresses.each do |address|
+        # now check the object
+        aidAddresses = aid.getAddresses
+        aidAddresses = aidAddresses.reverse
+        count = 0
+        addresses.each do |address|
                         assert(aidAddresses.include?(address), "Address: #{address} included")
-		end
-		
-		count = 0
-		aidResolvers = aid.getResolvers
-		aidResolvers = aidResolvers.reverse
+        end
 
-		resolvers.each do |resolver|
+        count = 0
+        aidResolvers = aid.getResolvers
+        aidResolvers = aidResolvers.reverse
+
+        resolvers.each do |resolver|
                         assert(aidResolvers.include?(resolver), "aidResolver should contain: #{resolver.getName} was found")
                 end
-	end
+    end
 
-	def test_FipaMessage
-		performative = :inform
-		msg = ACLMessage.new
-		msg.setPerformative(performative)
+    def test_FipaMessage
+        performative = :inform
+        msg = ACLMessage.new
+        msg.setPerformative(performative)
 
-		10.times do |count|
-			agent = AgentId.new("receiver-#{count}")
-			msg.addReceiver(agent)
-		end
-	
-		assert_equal(msg.getPerformative, performative)	
+        10.times do |count|
+            agent = AgentId.new("receiver-#{count}")
+            msg.addReceiver(agent)
+        end
 
-		count = 0
-		receivers = msg.getReceivers
-		receivers = receivers.reverse
-		receivers.each do |rcv|
-			assert_equal("receiver-#{count}", rcv.getName)
-			count += 1
-		end
+        assert_equal(msg.getPerformative, performative)
+
+        count = 0
+        receivers = msg.getReceivers
+        receivers = receivers.reverse
+        receivers.each do |rcv|
+            assert_equal("receiver-#{count}", rcv.getName)
+            count += 1
+        end
 
                 msg.clearReceivers
                 assert(msg.getReceivers.empty?, "Receivers should be empty after calling clearReceivers")
@@ -147,20 +147,20 @@ class FipaMessageTest < Minitest::Test
         assert(sender == e_sender, "Senders: #{sender}")
     end
 
-	def test_InterfaceCalls
- 		msg = ACLMessage.new
-		msg.setPerformative :request
- 		msg.getPerformative
-		msg.setContent ""
-		msg.getContent
-	end
+    def test_InterfaceCalls
+        msg = ACLMessage.new
+        msg.setPerformative :request
+        msg.getPerformative
+        msg.setContent ""
+        msg.getContent
+    end
 
-	def test_ByteVector
-		msg = ACLMessage.new
-		msg.setPerformative :inform
-		msg.setContent ""
-		content = msg.to_byte_array
-		p content
-	end
+    def test_ByteVector
+        msg = ACLMessage.new
+        msg.setPerformative :inform
+        msg.setContent ""
+        content = msg.to_byte_array
+        p content
+    end
 end
 
